70a3c679c33eebc9e1dcfc76b009eda2ebd90464
Medina <medina.ahmed.mo@gmail.com>
2023-10-25 13:09:36 -0400

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
 import './App.css'
 
-import NavBar from "./components/NavBar.js"
-import  Hero  from "./components/Hero.js";
-import About from "./components/About.js"
-import Work from "./components/Work.js"
+import ComingSoon from "./components/ComingSoon.js"
+
 
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <NavBar/>
-        <Hero/>
-        <About/>
-        <Work/>
+        <ComingSoon/>
       </div>
     );
   }
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="Hero">
+      <section className="hero">
+        <header className="header">
+          <h1>MEDINA MOHAMED</h1>
+          <h2>Coming soon</h2>
+        </header>
+      </section>
+
+    </div>
+  );
+};
+
+
+export default ComingSoon;

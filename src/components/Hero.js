6b7daaec01f721608e77f5f141977483af5a20eb
Medina <medina.ahmed.mo@gmail.com>
2023-10-28 11:03:24 -0400

    fixed typo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <header className="header">
           <h2>Hi, I am Medina</h2>
           <h1>DEVELOPER</h1>
-          <p>Aspiring web developer with a passion for harmonizing design and code..</p>
+          <p>Aspiring web developer with a passion for harmonizing design and code.</p>
         </header>
       </section>
 

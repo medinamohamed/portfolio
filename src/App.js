bd1c86b3c97711fc58d8a5f3ceffeb82a04e8b6f
Medina <medina.ahmed.mo@gmail.com>
2024-02-01 15:36:50 -0500

    coming-soon
    coming-soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, {Component} from 'react';
-import './App.css'
+import React, { useEffect, useRef } from 'react';
+import './App.css';
 
-import NavBar from "./components/NavBar.js"
-import  Hero  from "./components/Hero.js";
-import About from "./components/About.js"
-import Work from "./components/Work.js"
+import NavBar from './components/NavBar.js';
+import Hero from './components/Hero.js';
+import About from './components/About.js';
+import Work from './components/Work.js';
+import ComingSoon from './components/ComingSoon.js';
 
+const App = () => {
+  const appRef = useRef(null);
+  const toRef = useRef(null);
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <NavBar/>
-        <Hero/>
-        <About/>
-        <Work/>
-      </div>
-    );
-  }
-}
+  useEffect(()=> {
+    const moveGradient = (event) => {
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+ 
+      // const mouseX = Math.round(event.pageX/ winWidth) *100))
+    }
+  })
+
+  return (
+    <div className="App">
+      <ComingSoon />
+    </div>
+  );
+};
 
 export default App;

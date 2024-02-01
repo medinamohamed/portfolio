1dd60738b1a73632eec3564ef8a867145d2547de
Medina <medina.ahmed.mo@gmail.com>
2024-02-01 15:40:04 -0500

    fixed failed build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
 
-import NavBar from './components/NavBar.js';
-import Hero from './components/Hero.js';
-import About from './components/About.js';
-import Work from './components/Work.js';
+// import NavBar from './components/NavBar.js';
+// import Hero from './components/Hero.js';
+// import About from './components/About.js';
+// import Work from './components/Work.js';
 import ComingSoon from './components/ComingSoon.js';
 
 const App = () => {
   const appRef = useRef(null);
   const toRef = useRef(null);
 
-  useEffect(()=> {
-    const moveGradient = (event) => {
-      const winWidth = window.innerWidth;
-      const winHeight = window.innerHeight;
+  // useEffect(()=> {
+  //   const moveGradient = (event) => {
+  //     const winWidth = window.innerWidth;
+  //     const winHeight = window.innerHeight;
  
-      // const mouseX = Math.round(event.pageX/ winWidth) *100))
-    }
-  })
+  //     // const mouseX = Math.round(event.pageX/ winWidth) *100))
+  //   }
+  // })
 
   return (
     <div className="App">
